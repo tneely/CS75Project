69fb9dc93ac25240520921afbe246c7e68e3dbc7
Taylor Neely <tneely417@gmail.com>
2015-11-01 09:48:28 -0500

    added edge class to graph

--- Graph.py
+++ Graph.py
@@ -22,9 +22,9 @@ class Graph:
 ### NODE CLASS ###
 	class Node:
 
-		def __init__(self, contents, adjacents = None):
+		def __init__(self, contents, adjacents = None): # Not sure what the best way to track adj is, within node or in edge class?
 			self.contents = contents
-			self. adjacents = adjacents
+			self.adjacents = adjacents
 
 		def __getitem__(self, n):
 	        """The nth adjacent node in
@@ -35,7 +35,7 @@ class Graph:
 	        return self.adjacents[pos]
 
 	    def __len__(self):
-	        """The number of adjacents
+	        """The number of adjacent nodes
 	        Returns:
 	          int
 	        """
@@ -44,13 +44,70 @@ class Graph:
 	    def __str__(self):
 	        """The contents of the node"""
 	        return self.contents
+
+	    def add_adj(self, other):
+	    	"""Adds other nodes to 
+	    		adjacent list of this
+	    		node, and vice versa
+	    	"""
+	    	self.adjacents.append(other)
+	    	other.adjacents.append(self)
 ### NODE CLASS ###
 ##################
 
-    def __init__(self, stuff):
-        self.stuff = stuff
+##################
+### EDGE CLASS ###
+	class Edge:
+
+		def __init__(self, contents = None, node1, node2):
+			self.contents = contents
+			self.adjacents = [node1, node2]
+
+		def __getitem__(self, n):
+	        """The nth adjacent node in
+	        	the adjacents list [0,1]
+	        Returns:
+	          Object: Node
+	        """
+	        return self.adjacents[pos]
+
+	    def __len__(self):
+	        """The number of adjacents
+	        Returns:
+	          int (2)
+	        """
+	        return len(self.adjacents)
+
+	    def __str__(self):
+	        """The contents of the edge"""
+	        return self.contents
+### EDGE CLASS ###
+##################
+
+    def __init__(self):
+        self.nodeList = []
+        self.edgeList = []
         
     def __str__(self):
     	"""Print out nodes and 
     		their adjacents? 
     	"""
+    	pass
+
+    def new_node(self, contents, adjacents = None):
+    	node = Node(contents, adjacents)
+    	self.nodeList.append(node)
+
+    	return node
+
+    def new_edge(self, contents = None, node1, node2):
+    	edge = Edge(contents, node1, node2)
+    	self.edgeList.append(edge)
+
+    	return edge
+
+    def del_node(self, node):
+    	nodeList.remove(node)
+
+    def del_edge(self, edge):
+    	edgeList.remove(edge)
